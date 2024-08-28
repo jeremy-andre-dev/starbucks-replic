cdc7946acd6b706031d12cc9bf7d656d76b23f13
jeremy-andre-dev <jeremy.andre.dev@gmail.com>
2024-08-28 03:47:19 -0500

    add link

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,11 +1,19 @@
 import { PiMapPinFill } from "react-icons/pi";
+import { IoLogoLinkedin } from "react-icons/io5";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
     <div className="fixed flex w-full flex-col items-center bg-white shadow-md">
-      <div className="flex w-full justify-center text-white bg-black px-6 py-1 font-semibold text-[0.9rem] text-center">
-        Esta página es un proyecto personal creado para demostrar mis
-        habilidades como desarrollador. No es la página oficial de Starbucks.
+      <div className="flex w-full flex-wrap items-center justify-center gap-2 bg-black px-6 py-1 text-center text-[0.9rem] font-semibold text-white">
+        <p>
+          Esta página es un proyecto personal creado para demostrar mis
+          habilidades como desarrollador. No es la página oficial de Starbucks.
+        </p>
+        <Link href={"https://www.linkedin.com/in/jeremyandre99/"} target="_blank" className="flex cursor-pointer items-center gap-1 text-[0.8rem] font-medium text-green-500">
+          <IoLogoLinkedin size="1rem" />
+          <p>Jeremy.dev</p>
+        </Link>
       </div>
       <div className="flex w-full max-w-[94rem] justify-between px-6 py-6 text-[rgba(0,0,0,0.87)]">
         {/* left NavBar _____________________________*/}
